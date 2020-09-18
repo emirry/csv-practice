b55ef7befddbc2b56d2a94ba6143fbe603cc3b1f
Emily Nagai <emnagai89@gmail.com>
2020-09-18 12:24:04 -0700

    First method complete. Second method to be cont

--- lib/main.rb
+++ lib/main.rb
@@ -1,32 +1,31 @@
-require 'csv'
-require 'awesome_print'
-# all_fields = w%(Id Name Sex Age Height Weight Team NOC Games Year Season City Sport Event Medal)
-# required_olympian_fields = w%(Id Name Height Team Year City Sport Event Medal)
+require "awesome_print"
+require "csv"
+
 def get_all_olympic_athletes(filename)
   olympic_athlete = CSV.read(filename, headers: true).map(&:to_h)
   required_olympian_fields = %w(ID Name Height Team Year City Sport Event Medal)
 
   olympic_athlete.each do |athlete_array|
-    athlete_array.select! { |category, value| required_olympian_fields.include?(category) }
+    athlete_array.select! { |category, value| required_olympian_fields.include?(category)}
+    puts olympic_athlete
+    ap olympic_athlete
   end
-
   return olympic_athlete
 end
-# p get_all_olympic_athletes('../data/athlete_events.csv')
+
+# olympic_athletes = get_all_olympic_athletes('../lib/sample.csv')
 
 def total_medals_per_team(olympic_data)
-  expected_totals = 0
+  # olympic_athletes
+ expected_totals = 0
   medals_per_team_hash = {}
   olympic_athletes = get_all_olympic_athletes('../lib/sample.csv')
 
   olympic_athletes.each do |athlete, value|
     puts "#{athlete["City"]} => #{value}"
-  end
+    end
 
-  return expected_totals
-
-end
-
-def get_all_gold_medalists(olympic_data)
 
 end
+
+p total_medals_per_team('../lib/sample.csv')
